5443e3163305037e4ecfc0921451f032b758315e
Lee-GwangCheon <gclee@seerealbox.com>
2024-05-08 15:26:37 +0900

    feat: Add collection feeds and  users

--- config/mongodb.py
+++ config/mongodb.py
@@ -1,7 +1,13 @@
-from motor.motor_asyncio import AsyncIOMotorClient
+from motor.motor_asyncio import (
+    AsyncIOMotorClient,
+    AsyncIOMotorDatabase,
+    AsyncIOMotorCollection,
+)
 
 from config.settings import setting
 
 
 mongodb_client = AsyncIOMotorClient(setting.mongodb_url)
-sereal_database = mongodb_client.get_database("seereal")
+sereal_database: AsyncIOMotorDatabase = mongodb_client.get_database("seereal")
+feeds_collection: AsyncIOMotorCollection = sereal_database.get_collection("feeds")
+users_collection: AsyncIOMotorCollection = sereal_database.get_collection("users")
